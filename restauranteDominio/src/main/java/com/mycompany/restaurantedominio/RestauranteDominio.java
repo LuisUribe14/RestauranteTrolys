32e29e533af2269a6d7d9b70833340bd5a5d9dfc
LuisUribe14 <luis.uribe253239@potros.itson.edu.mx>
2025-04-03 01:50:38 -0700

    Inicio

---
+++ restauranteDominio/src/main/java/com/mycompany/restaurantedominio/RestauranteDominio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.restaurantedominio;
+
+/**
+ *
+ * @author multaslokas33
+ */
+public class RestauranteDominio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
